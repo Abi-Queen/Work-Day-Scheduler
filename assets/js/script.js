a2788baf0054b44a58288c8457ef34decadd2601
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-08-03 11:07:31 -0700

    initial files, bootstrap grid layout, sync html with existing css classes, commented plan in js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// get current date
+// display date in jumbotron
+
+// set up bootstrap grid layout
+
+// add hour labels in left column
+
+// add event listeners to grid boxes
+
+// dynamically generate html content in grid boxes
