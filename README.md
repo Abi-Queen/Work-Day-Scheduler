97fa454206b8a0abc952ca9b549d5fc879e1c52f
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-08-09 05:35:57 -0700

    added screenshot image, edited readme, finished edits from tutor input

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Work-Day-Scheduler
+# Work Day Scheduler
+![screenshot of app](./assets/images/work-day-scheduler-screenshot.jpg)
+
+## Description
+This app allows users to enter, edit, and save events for each hour of the work day. The display indicates past, present, and future time blocks by changing background color. Here is a link to the [deployed page.](https://abi-queen.github.io/Password-Generator/)
+
+## Contributors
+I built this app with HTML and CSS starter code from the UC Davis Bootcamp (2022). Tutor Mansi Patel helped me edit some of the JavaScript and JQuery code.
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,105 +9,37 @@ console.log(currentTime)
 
 currentDate.textContent = currentTime.format("dddd, MMMM Do, YYYY");
 
+// create current hour and time block variables; check time block ids against hour of current time and change backgrounds for past, present, future
 
-// change col color if past, present, future
-var background = function() {
-    if ((moment().hour()) == ($(".col-8 desc").id.value)) {
-        $(".col-8 desc").addClass("bg-danger");
+$(".time-block").each(function(){
+   var currentHour = moment().hours(); 
+   var blockHour = parseInt($(this).attr("id"));
+   console.log("Block Hour: "+blockHour);
+
+    if(blockHour < currentHour){
+        $(this).addClass("past")
     }
-    else if ((moment().hour()) < ($(".col-8 desc").id.value)) {
-        $(".col-8 desc").addClass("bg-success");
+    else if(blockHour === currentHour){
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    }else{
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
     }
-    else if ((moment().hour()) > ($(".col-8 desc").id.value)) {
-        $(".col-8 desc").addClass("bg-secondary");
-    } 
-};
+})
+
+// add event listener to saveBtn; save changes to descriptions in localStorage with id attributes
 console.log(moment().hour());
+$(".saveBtn").on("click", function(){
+    var note = $(this).siblings(".desc").val();
+    var hour = $(this).parent().attr("id");
 
+    localStorage.setItem(hour, note);
+})
 
-// change saveBtn icon to unlocked if editing 
-// need separate js for for each row?
-$(".col-8 desc").change(function() {
-    $(".saveBtn-").addClass("iconic-lock-unlocked");
-});
+// create for loop of description content using hour block ids
+for(var i=9; i<18;i++){
+    $("#"+i).children(".desc").val(localStorage.getItem(i));
+}
 
-
-// add description input
-// start with eventListener "click"
-$(".col-8 desc").on ("click", "p", function() {
-    var text = $(this).text().trim();
-    var textInput = $("<textarea>")
-    .addClass("desc-item").val(text);
-    $(this).replaceWith(textInput);
-    textInput.trigger("focus");
-});
-
-// save by clicking off input area
-$(".col-8 desc").on("blur", "textarea", function() {
-
-// get the textarea's current value
-var text = $(this).val().trim();
-
-//get parent's id attribute
-var status = $(this).closest(".col-8 desc").attr("id").replace("col-", "");
-
-// get description's position in the list
-var index = $(this).closest(".desc-item").index();
-
-desc[status][index].text = text;
-saveDesc();
-
-//recreate p element
-var descP = $("<p>").addClass("m-1").text(text);
-
-//replace textarea with p element
-$(this).replaceWith(descP);
-});
-
-// Save to local storage on each eventListener, using stringify
-var saveDesc = function() {
-localStorage.setItem("desc-item", JSON.stringify(desc));
-};
-
-// edit saved descriptions
-//description box was clicked
-$(".desc-item").on("click", "span", function() {
-    // get current text
-    var item = $(this).text().trim();
-
-    //create new input element
-    var itemInput = $("<input>")
-    .attr("type", "text")
-    .addClass("desc-item").val(item);
-
-    //swap out elements
-    $(this).replaceWith(itemInput);
-
-    //automatically focus on new element
-    itemInput.trigger("focus");
-});
-
-//description text was changed
-$(".desc-item").on("blur", "input[type='text']", function() {
-    //get current text
-    var item = $(this).val().trim();
-
-    //get parent's id attribute
-    var status = $(this).closest(".desc-item").attr("id").replace("desc-", "");
-
-    //get item's position in list
-    var index = $(this).closest(".desc-item").index();
-
-    //update item in array and re-save to localStorage
-    item[status][index].item = item;
-    saveDesc();
-
-    //recreate span element with bootstrap classes
-    var descSpan = $("<span>")
-    .addClass("desc-item").text(item);
-
-    //replace input with span element
-    $(this).replaceWith(descSpan);
-});
-
-//ended on mod 5.1.8
